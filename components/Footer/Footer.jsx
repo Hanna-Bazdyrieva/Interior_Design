f94efb83c58229d52a8b5da8d731d55592b5d0fa
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-11-02 23:07:29 +0100

    Footer changed

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,23 +4,84 @@ import { Socials } from "..";
 
 function Footer() {
 	return (
-		<footer className={`${s.section} section`} id="contacts">
-			<div className="container">
-				<div className={s.headerContainer}>
-					<h3 className={s.header}>For more details &nbsp;</h3>
-					<h3 className={s.header}>and to order a project:</h3>
+		<>
+			<footer className={`${s.section} section`} id="contacts">
+				<div className={`${s.container} container`}>
+					<div className={s.imgWrap}>
+						<img className={s.image} src="https://fakeimg.pl/100x130" alt="" />
+						<img className={s.image} src="https://fakeimg.pl/100x130" alt="" />
+						<img className={s.image} src="https://fakeimg.pl/100x130" alt="" />
+					</div>
+					<div className={s.textContainer}>
+						<h3 className={s.text}>For more details &nbsp;</h3>
+						<h3 className={s.text}>and to order a project:</h3>
+					</div>
+					<div className={s.wrap}>
+						<ul className={s.contact}>
+							<li>
+								<a className={s.contactLink} href="mailto: abc@example.com">
+									abc@example.com
+								</a>
+							</li>
+							<li>
+								<a
+									className={`${s.contactLink} ${s.navLink}`}
+									href="tel:+000000000000"
+								>
+									+000000000000
+								</a>
+							</li>
+							<li className={s.hours}>
+								<div className={s.greenCircle}></div>
+								<p className={s.hoursText}>
+									<span>Mon-Fri</span> <span>9:00am - 4:00pm</span>
+								</p>
+							</li>
+							<li>
+								<Socials />
+							</li>
+						</ul>
+
+						<nav className={s.navigation}>
+							<ul className={s.navList}>
+								<li>
+									<a href="#main" className={s.navLink}>
+										Home
+									</a>
+								</li>
+								<li>
+									<a href="#portfolio" className={s.navLink}>
+										Portfolio
+									</a>
+								</li>
+								<li>
+									<a href="#stages" className={s.navLink}>
+										Stages
+									</a>
+								</li>
+								{/* <li>
+									<a href="#prices" className={s.navLink}>
+										Prices
+									</a>
+								</li> */}
+								<li>
+									<a href="#contacts" className={s.navLink}>
+										Contacts
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div className={s.logo}>
+						<a href="#main" className={s.navLink}>
+							<h3 className={s.logoName}>Oleksandra Vasylevska</h3>
+							<p className={s.logoTitle}>interior design</p>
+						</a>
+					</div>
 				</div>
-				<div className={s.contact}>
-					<a className={s.contactLink} href="tel:+000000000000">
-						+000000000000
-					</a>
-					<a className={s.contactLink} href="mailto: abc@example.com">
-						abc@example.com
-					</a>
-				</div>
-				<Socials />
-			</div>
-		</footer>
+			</footer>
+			<section className={` ${s.footerImgSection}`} />
+		</>
 	);
 }
 
--- components/Socials/Socials.jsx
+++ components/Socials/Socials.jsx
@@ -22,7 +22,14 @@ function Socials() {
 				<li className={s.socialsItem}>
 					<a href="" className={s.socialsLink}>
 						<svg className={s.socialsIcon} width="20" height="20">
-							<use href="/icons/icons.svg#twitter"></use>
+							<use href="/icons/icons.svg#whatsapp"></use>
+						</svg>
+					</a>
+				</li>
+				<li className={s.socialsItem}>
+					<a href="" className={s.socialsLink}>
+						<svg className={s.socialsIcon} width="20" height="20">
+							<use href="/icons/icons.svg#telegram"></use>
 						</svg>
 					</a>
 				</li>
